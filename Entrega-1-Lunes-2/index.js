c91dd9778542aca8fd02cbb0516a12f8f73ebe72
elEma89 <61033552+elEma89@users.noreply.github.com>
2025-06-03 00:02:25 -0400

    Add files via upload

---
+++ Entrega-1-Lunes-2/index.js
@@ -0,0 +1,11 @@
+import ProductManager from "./ProductManager.js";
+
+const ProductManager = new ProductManager("./products.json");
+
+const main = async() => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+    }
+}
