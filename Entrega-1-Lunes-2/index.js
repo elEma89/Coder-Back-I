ede1737b92dac3f7ae285f738c5d9dac4af3dce4
elEma89 <61033552+elEma89@users.noreply.github.com>
2025-07-07 19:10:35 -0400

    Update index.js

--- Entrega-1-Lunes-2/index.js
+++ Entrega-1-Lunes-2/index.js
@@ -1,11 +1,55 @@
-import ProductManager from "./ProductManager.js";
+//Conectar websockets del lado cliente 
+const socket = io();
 
-const ProductManager = new ProductManager("./products.json");
+const formNewProduct = document.getElementById("formNewProduct");
+const productsList = document.getElementById("productsList");
 
-const main = async() => {
-    try {
+formNewProduct.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    } catch (error) {
-        console.log(error);
+    const formData = new FormData(formNewProduct);
+    const productData = {};
+
+    formData.forEach((value, key) => {
+        productData[key] = value;
+    });
+
+    //Enviar los datos del producto al servidor
+    socket.emit("newProduct", productData);
+
+});
+
+
+socket.on("productAdded", (newProduct) => {
+
+    productsList.innerHTML += 
+    `<div  id="productsList" class="col" >
+        <div class="card h-100" style="width: 18rem;" data-id="${newProduct.id}">
+        <img src="${newProduct.thumbnail}" class="card-img-top" alt="${newProduct.title}">
+        <div class="card-body">
+            <h5 class="card-title">${newProduct.title}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">${newProduct.category}</h6>
+            <p class="card-text">${newProduct.description}</p>
+            <h6 class="card-subtitle mb-2 text-body-secondary">
+            $${Number(newProduct.price).toLocaleString("es-AR")}
+            </h6>
+            <button class="btn btn-danger btn-delete" data-id="${newProduct.id}">Eliminar</button>
+            <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+        </div>
+    </div>`;
+});
+
+//Eliminar producto
+productsList.addEventListener("click", e => {
+    if (e.target.matches(".btn-delete")) {
+        const id = e.target.dataset.id;
+        socket.emit("deleteProduct", id);
     }
-}
+});
+
+socket.on("productDeleted", productId => {
+    const card = productsList.querySelector(`[data-id="${productId}"]`);
+    if (card) card.remove();
+});
+
